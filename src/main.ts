573b5d2a8d6105b51a42ba4f2cef1e7a1bd7e750
kimi <kljeung@ucsc.edu>
2025-10-09 16:20:35 +0000

    Added button and header (step 1)

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-   <button id="iconButton" class="icon-button">
-    <img src="${exampleIconUrl}" class="icon" />
-  </button>
-  <div id="output"></div>
+  <h1> Roachfiestation </h1>
+  <button id = "button">🪳</button>
+  
+  <div id = "output"></div>
 `;
 
-const button = document.getElementById("circleButton");
-const output = document.getElementById("output");
+const button = document.getElementById("button");
 
-console.log("this is for section :)");
-
-if (button && output) {
+if (button) {
   button.addEventListener("click", () => {
-    output.textContent = "wow you actually clicked this..";
+    console.log("temp");
   });
 }
